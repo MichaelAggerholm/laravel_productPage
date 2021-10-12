79939127a0f60c897f7d972c8cfbad3f44e5a756
ma <92aggerholm@gmail.com>
2021-10-12 19:29:58 +0200

    created products migration and crud functions for this.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('products', ProductController::class);
 
 Auth::routes();
 
